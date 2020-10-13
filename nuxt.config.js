874de73923eb2a814a572f10c5fe69b817482df3
sancyo <b1845958@planet.kanazawa-it.ac.jp>
2020-10-13 15:55:17 +0900

    add:共通のCSSを追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['normalize.css'],
+  css: ['normalize.css', '@/config/common.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
