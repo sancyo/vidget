bc10753edcabae5e417ff85baaabd758eb2864d6
sancyo <b1845958@planet.kanazawa-it.ac.jp>
2020-10-16 01:06:02 +0900

    style:フッターのスタイルを調整

--- components/molecules/pageFooter.vue
+++ components/molecules/pageFooter.vue
@@ -1,10 +1,17 @@
 <template>
   <footer class="page-footer">
     <ul class="page-footer-inner">
-      <li class="footer-item"><a href="">利用規約</a></li>
-      <li class="footer-item"><a href="">プライバシーポリシー</a></li>
-      <li class="footer-item"><a href="">お問い合わせ</a></li>
+      <li class="footer-item">
+        <a href="" class="footer-item-link">Terms</a>
+      </li>
+      <li class="footer-item">
+        <a href="" class="footer-item-link">Privacy</a>
+      </li>
+      <li class="footer-item">
+        <a href="" class="footer-item-link">Contact</a>
+      </li>
     </ul>
+    <p class="copyright">@Copyright 2020, vidget All rights resrved</p>
   </footer>
 </template>
 
@@ -15,13 +22,24 @@ export default {}
 <style scoped>
 .page-footer {
   padding: 3.2rem 2.2rem;
+  background: #323232;
 }
 .page-footer-inner {
   display: flex;
   justify-content: center;
+  padding: 0.6rem 0;
 }
 .footer-item {
   margin-right: 1.2rem;
   font-size: 1.2rem;
+}
+.footer-item-link {
+  color: #fff;
+}
+.copyright {
+  text-align: center;
+  padding: 1.2rem 0;
+  font-size: 1.2rem;
+  color: #bababa;
 }
 </style>
